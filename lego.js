1eb4b285a56fe7795e7316772167ba76a1ed0138
𝕊𝕥𝕒𝕣 𝕎𝕒𝕣𝕤 𝔸𝕟𝕚𝕞𝕒𝕥𝕚𝕠𝕟𝕤 𝟜𝟜 <113991193+starwarsanimations44@users.noreply.github.com>
2025-05-06 00:53:17 +0200

    Update lego.js

--- lego.js
+++ lego.js
@@ -1,10 +1,10 @@
 function initialize() {
-	document.querySelectorAll("a").forEach(
+	/* document.querySelectorAll("a").forEach(
 		a => {
 			a.href =
 				"https://www.lego.com/en-us/service/buildinginstructions/" +
 				a.href.split("/")[a.href.split("/").length() - 1]
 			;
 		}
-		);
+	); */
 }
